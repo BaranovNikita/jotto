f611307e89772b6eada472e2468c8b98d6d0ec4a
Никита Баранов <baranov.nikita@gmail.com>
2020-06-14 01:21:24 +0300

    congrats green tests

--- src/Congrats.js
+++ src/Congrats.js
@@ -3,9 +3,19 @@ import React from 'react'
 /**
  * Functional component for congratulatory message
  * @function
+ * @param {object} props - React props
  * @returns {JSX.Element} - Rendered component (or null if 'success' prop is false)
  * @constructor
  */
-export const Congrats = () => (
-  <div />
-)
+export const Congrats = (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">Congratulations! You guessed the word!</span>
+      </div>
+    )
+  }
+  return (
+    <div data-test="component-congrats" />
+  )
+}
